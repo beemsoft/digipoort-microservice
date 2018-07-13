cd3a6bf8febafa3f3ecef6a2d9d1e13d87cca0dc
Hans Beemsterboer <hpbeem@gmail.com>
2018-07-13 13:03:25 +0200

    Return status response as JSON string

--- src/main/java/org/techytax/digipoort/lambda/DigipoortLambdaHandler.java
+++ src/main/java/org/techytax/digipoort/lambda/DigipoortLambdaHandler.java
@@ -2,13 +2,13 @@ package org.techytax.digipoort.lambda;
 
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.techytax.digipoort.DigipoortService;
 import org.techytax.digipoort.DigipoortServiceImpl;
 import org.techytax.digipoort.lambda.event.LambdaEvent;
 import org.techytax.ws.AanleverResponse;
 import org.techytax.ws.AanleverServiceFault;
 import org.techytax.ws.GetStatussenProcesResponse;
-import org.techytax.ws.StatusResultaat;
 import org.techytax.wus.status.StatusinformatieServiceFault;
 
 import java.io.IOException;
@@ -17,6 +17,7 @@ import java.security.GeneralSecurityException;
 public class DigipoortLambdaHandler implements RequestHandler<LambdaEvent, String> {
 
   private DigipoortService digipoortService = new DigipoortServiceImpl();
+  private ObjectMapper objectMapper = new ObjectMapper();
 
   @Override
   public String handleRequest(LambdaEvent input, Context context) {
@@ -31,12 +32,7 @@ public class DigipoortLambdaHandler implements RequestHandler<LambdaEvent, Strin
   private String handleCheckStatus(LambdaEvent input) {
     try {
       GetStatussenProcesResponse response = digipoortService.getStatussenProces(input.getMode(), input.getCheckStatus().getReferenceId());
-      for (StatusResultaat resultaat : response.getGetStatussenProcesReturn().getStatusResultaat()) {
-        if (resultaat.getStatuscode().equals("400")) {
-          return "OK";
-        }
-      }
-      return "NOT OK";
+      return objectMapper.writeValueAsString(response);
     } catch (IOException | StatusinformatieServiceFault | GeneralSecurityException e) {
       e.printStackTrace();
       return e.getMessage();
